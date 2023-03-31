809bfe4f667a48e0b4a535b4c0974426909ff5ff
angry-dev <98198645+angry-dev@users.noreply.github.com>
2023-03-31 17:02:04 +0900

    [Feat] Model 파일 생성

---
+++ Glossy/Model/Comment.swift
@@ -0,0 +1,14 @@
+//
+//  Comment.swift
+//  Glossy
+//
+//  Created by Hyesung Jeon on 2023/03/31.
+//
+
+import Foundation
+
+struct Comment {
+    var userId: String // 유저id
+    var postId: String // 게시물id
+    var commentImage: [String] // 댓글 이미지
+}
