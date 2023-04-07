846137d5b2524ed9f3909b708210135a2bab83dc
angry-dev <98198645+angry-dev@users.noreply.github.com>
2023-04-07 16:38:29 +0900

    [Feat] 뷰모델 파일 생성 및 뷰 구성
    - 뷰모델 파일 생성
    - FeedCollectionViewCell 구성
    - 기본 프로필 이미지 적용

--- Glossy/View/FeedViewController.swift
+++ Glossy/View/FeedViewController.swift
@@ -7,23 +7,75 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+struct Test {
+    let title: String
+}
 
+extension Test {
+    static var data = [
+        Test(title: "MBTI"),
+        Test(title: "에고그램 테스트"),
+        Test(title: "mgram"),
+        Test(title: "애착유형 테스트"),
+        Test(title: "EQ 테스트"),
+        Test(title: "연애 능력 테스트"),
+        Test(title: "연락 유형 테스트"),
+        Test(title: "결혼상대 테스트"),
+    ]
+}
+
+class FeedViewController: UIViewController {
+    
+    let cellName = "CustomFeedCollectionViewCell"
+    let cellReuseIdentifier = "customFeedCell"
+    let testList = Test.data
+    let sectionInsets = UIEdgeInsets(top: 10, left: 10, bottom: 80, right: 10)
+
+    private func registerXib() {
+        let nibName = UINib(nibName: cellName, bundle: nil)
+        collectionView.register(nibName, forCellWithReuseIdentifier: cellReuseIdentifier)
+    }
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.registerXib()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension FeedViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return testList.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as?
+                   CustomFeedCollectionViewCell else { return UICollectionViewCell() }
+        
+        let cellData = self.testList[indexPath.row]
+        
+        cell.userNickName?.text = cellData.title
+        cell.profileImage.image = UIImage(named: "default_profile_image")
+        
+        
+        
+        return cell
+    }
+}
 
+extension FeedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.bounds.width, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.bottom
+    }
 }
