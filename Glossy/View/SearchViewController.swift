bb6f3591d4106eae736d50ff3ef111df25348179
angry-dev <98198645+angry-dev@users.noreply.github.com>
2023-04-07 17:38:46 +0900

    [Fix] 머지 이슈 해결

--- Glossy/View/SearchViewController.swift
+++ Glossy/View/SearchViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundColor = .secondarySystemBackground
+        return searchBar
+    }()
+    private var collectionView: UICollectionView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.topItem?.titleView = searchBar
 
-        // Do any additional setup after loading the view.
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        guard let collectionView = collectionView else {
+            return
+
+        }
+        view.addSubview(collectionView)
+
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView( _ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
     }
-    */
 
+    func collectionView( _ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
 }
--- Glossy/ViewModel/CreatePostViewModel.swift
+++ Glossy/ViewModel/CreatePostViewModel.swift
@@ -6,7 +6,24 @@
 //
 
 import Foundation
+import FirebaseStorage
 
-class CreatePostViewModel {
-    
+public class CreatePostViewModel {
+
+    private let storage = Storage.storage().reference()
+
+    public func uploadUserPost(model: Post, completion: (Result<URL,Error>) -> Void){
+
+    }
+
+    public func downloadImage(with reference: String,  completion: (Result<URL,Error>) -> Void){
+
+    }
+    public enum PostType{
+        case photo, video
+    }
+    public struct Post{
+        let postType: PostType
+    }
+
 }
