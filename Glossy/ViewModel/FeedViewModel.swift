bf29bb8e0c1c2c6cf7134a02183d0192ac4a79e1
angry-dev <98198645+angry-dev@users.noreply.github.com>
2023-04-19 15:52:45 +0900

    [Fix] Remove Stroyboard Add SnapKit
    - 스토리보드 삭제
    - SnapKit import
    - initalViewController 수정
    - 탭바컨트롤러 구현

--- Glossy/ViewModel/FeedViewModel.swift
+++ Glossy/ViewModel/FeedViewModel.swift
@@ -8,6 +8,16 @@
 import Foundation
 import FirebaseFirestore
 
+// firestore에서 데이터를 가져온다
+// post 객체를 만들어주고 posts 배열에 넣어준다
 class FeedViewModel {
     
+    // 게시물들
+    var posts: [Post] = []
+    
+    // FireStore 참조 객체
+    let database = Firestore.firestore()
+    
+
+
 }
